127185867a5ca013f9dd56a00ac970c7ddbd2661
nerotvlive <daniel.niesmann@outlook.com>
2024-04-01 23:11:59 +0200

    Initial commit

---
+++ assets/js/nexus-app.js
@@ -0,0 +1,11 @@
+function hideApp() {
+    window.location.href = "../application.html"
+}
+
+function minimizeApp() {
+    window.location.href = ".."
+}
+
+function closeApp() {
+    window.location.href = "../index.html?closeApp=true"
+}
